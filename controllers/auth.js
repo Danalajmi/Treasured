ff3d079e32cd2274e40422a30fe70903dce6ca40
Your Username <youremail@domain.com>
2025-11-10 18:55:58 +0300

    build the sign in page

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,4 +27,7 @@ router.post('/sign-up', async (req, res) => {
     res.send('Form submission accepted')
 })
 
+router.get('/sign-in',  (req, res) => {
+    res.render('auth/sign-in.ejs')
+})
 module.exports = router
