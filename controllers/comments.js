aa5473efac2a48963d3f20ef8af342da1e7aa705
Danalajmi <danaralajami@gmail.com>
2025-11-12 18:06:59 +0300

    fix auth router

--- controllers/comments.js
+++ controllers/comments.js
@@ -3,10 +3,7 @@ const Comment = require('../models/comment');
 const Post = require('../models/post')
 
 exports.comment_index_get = async (req,res) => {
-
-  // this is for temp testing until show page is created, actual is:
-  // const postComments = await Comment.find({postID: req.params.postID}).populate('userID', 'postID')
-  const postComments = await Comment.find({postID: '6910b77224318adbf6796315'}).populate('userID', 'postID')
+  const postComments = await Comment.find({postID: req.params.postID}).populate('userID', 'postID')
   res.render('comments/show.ejs' , {postComments})
 }
 
@@ -17,14 +14,12 @@ exports.comment_new_get = async (req,res) => {
 exports.comment_new_post = async (req,res) => {
   await Comment.create({
     description: req.body.description,
-    // temp till auth is integrated
-    userId: req.session?.user?._id || new mongoose.Types.ObjectId()
+    userId: req.session.user._id
   })
-  // temp actual is; res.redirect(`/posts/${req.params.postID}`)
-  res.redirect(`/posts/6910b77224318adbf6796315`)
+  res.redirect(`/posts/${req.params.postID}`)
 }
 
 exports.comment_delete = async (req,res) => {
   await Comment.findByIdAndDelete(req.params.commentID)
-  res.redirect('/posts/')
+  res.redirect(`/posts/${req.params.postId}/comments`)
 }
